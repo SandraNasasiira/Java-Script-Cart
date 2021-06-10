1d3f0162a34677b79a3e64afe3b4ce4054f83b2e
Sandra Nasasiira <sandranasasiiraresty@gmail.com>
2021-06-10 19:18:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*function one1()
+{
+    return{
+        bar:"Hello"
+    };
+}function two2()
+{
+    return{
+        bar:"Hello"
+    };
+}*/
+//Both functions will not print any outcome because
